28a7a9415a23258f1d95f437bb86b7e8e2d984f9
renatarioss <gerson.castro70@gmail.com>
2019-10-28 21:02:07 -0300

    Sistema de controle de funcionarios WebAPI - versão 1.0

---
+++ Projeto.Services/Models/DependenteCadastroModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Services.Models
+{
+    public class DependenteCadastroModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        public int IdFuncionario { get; set; }
+    }
+}
---
+++ Projeto.Services/Models/DependenteEdicaoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projeto.Services.Models
+{
+    public class DependenteEdicaoModel
+    {
+        [Required]
+        public int IdDependente { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        public int IdFuncionario { get; set; }
+    }
+}
